cb0fef10d40d5c1b76b7455c745bcf2cdf20ced1
Brian Yau <brianyauu@gmail.com>
2024-04-26 15:52:19 +0800

    README and format change

--- sec-con.spec.js
+++ sec-con.spec.js
@@ -3,11 +3,11 @@ const SecCon = require("./sec-con");
 test("Valid input to construct SecCon", () => {
   expect(new SecCon(10)).toBeInstanceOf(SecCon);
   expect(new SecCon("00:00:10")).toBeInstanceOf(SecCon);
-  expect(new SecCon("20:30", "HH:MM")).toBeInstanceOf(SecCon);
-  expect(new SecCon("00:10", "MM:SS")).toBeInstanceOf(SecCon);
-  expect(new SecCon("90", "HH")).toBeInstanceOf(SecCon);
-  expect(new SecCon("20", "MM")).toBeInstanceOf(SecCon);
-  expect(new SecCon("1234567890", "SS")).toBeInstanceOf(SecCon);
+  expect(new SecCon("20:30", "H:M")).toBeInstanceOf(SecCon);
+  expect(new SecCon("00:10", "M:S")).toBeInstanceOf(SecCon);
+  expect(new SecCon("90", "H")).toBeInstanceOf(SecCon);
+  expect(new SecCon("20", "M")).toBeInstanceOf(SecCon);
+  expect(new SecCon("1234567890", "S")).toBeInstanceOf(SecCon);
 });
 
 test("Invalid input to construct SecCon class", () => {
@@ -17,10 +17,10 @@ test("Invalid input to construct SecCon class", () => {
   expect(() => new SecCon("1234567890")).toThrow();
   expect(() => new SecCon("01:90:00")).toThrow();
   expect(() => new SecCon("01:00:90")).toThrow();
-  expect(() => new SecCon("ABC", "SS")).toThrow();
-  expect(() => new SecCon("01:00:90", "MM:SS")).toThrow();
-  expect(() => new SecCon("00:90", "MM:SS")).toThrow();
-  expect(() => new SecCon("00:90", "HH:SS")).toThrow();
+  expect(() => new SecCon("ABC", "S")).toThrow();
+  expect(() => new SecCon("01:00:90", "M:S")).toThrow();
+  expect(() => new SecCon("00:90", "M:S")).toThrow();
+  expect(() => new SecCon("00:90", "H:S")).toThrow();
   expect(() => new SecCon("01:00:90", "XX")).toThrow();
 });
 
@@ -30,23 +30,23 @@ test("String input validation", () => {
 });
 
 test("Float number input", () => {
-  expect(new SecCon(3600.5).format("SS")).toBe("3600");
-  expect(new SecCon(10.1).format("SS")).toBe("10");
+  expect(new SecCon(3600.5).format("S")).toBe("3600");
+  expect(new SecCon(10.1).format("S")).toBe("10");
 });
 
 test("Formatting string", () => {
-  expect(new SecCon(10).format("SS")).toBe("10");
-  expect(new SecCon("00:00:10").format("SS")).toBe("10");
-  expect(new SecCon("00:01:00").format("MM")).toBe("1");
-  expect(new SecCon("01:00:00").format("HH")).toBe("1");
-  expect(new SecCon("01:01:01").format("SS")).toBe("3661");
-  expect(new SecCon("00:20:01").format("MM:SS")).toBe("20:01");
-  expect(new SecCon("01:01:01").format("MM:SS")).toBe("61:01");
-  expect(new SecCon("01:01:01").format("HH:MM")).toBe("1:01");
-  expect(new SecCon("01:01:01").format("HH:MM:SS")).toBe("1:01:01");
-  expect(new SecCon("00:02:01").format("MM:SS", 2)).toBe("02:01");
-  expect(new SecCon("01:01:01").format("HH:MM", 2)).toBe("01:01");
-  expect(new SecCon("01:01:01").format("HH:MM:SS", 2)).toBe("01:01:01");
+  expect(new SecCon(10).format("S")).toBe("10");
+  expect(new SecCon("00:00:10").format("S")).toBe("10");
+  expect(new SecCon("00:01:00").format("M")).toBe("1");
+  expect(new SecCon("01:00:00").format("H")).toBe("1");
+  expect(new SecCon("01:01:01").format("S")).toBe("3661");
+  expect(new SecCon("00:20:01").format("M:S")).toBe("20:01");
+  expect(new SecCon("01:01:01").format("M:S")).toBe("61:01");
+  expect(new SecCon("01:01:01").format("H:M")).toBe("1:01");
+  expect(new SecCon("01:01:01").format("H:M:S")).toBe("1:01:01");
+  expect(new SecCon("00:02:01").format("M:S", 2)).toBe("02:01");
+  expect(new SecCon("01:01:01").format("H:M", 2)).toBe("01:01");
+  expect(new SecCon("01:01:01").format("H:M:S", 2)).toBe("01:01:01");
 });
 
 test("Formatting string with invalid format", () => {
